964e1f57ac78fd9dad6264713fcf113eca2fa41c
Md-Sefat-Alam <md.sefatalam@gmail.com>
2023-11-18 17:23:50 +0600

    cleaning code

--- index.js
+++ index.js
@@ -11,24 +11,6 @@ app.use(cors());
 // Connect to SQLite database
 const db = new sqlite3.Database("./db/dua_main.sqlite");
 
-// const query = "SELECT name FROM sqlite_master WHERE type='table';";
-
-// // Execute the query
-// db.all(query, (err, tables) => {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     // Extract table names from the result
-//     const tableNames = tables.map((table) => table.name);
-
-//     // Log the table names
-//     console.log("Tables:", tableNames);
-//   }
-
-//   // Close the database connection
-//   db.close();
-// });
-
 // Define your API endpoints here
 app.get("/api/dua", (req, res) => {
   db.all("SELECT * FROM dua", (err, rows) => {
@@ -40,6 +22,82 @@ app.get("/api/dua", (req, res) => {
     }
   });
 });
+
+app.get("/api/dua/:cat_id", async (req, res) => {
+  const cat_id = req.params.cat_id;
+
+  try {
+    // Fetch subcategories for the given cat_id
+    const subcategories = await getSubcategories(cat_id);
+
+    // Fetch duas for the given cat_id
+    const duas = await getDuas(cat_id);
+
+    // Organize the data by subcategory
+    const organizedData = organizeData(subcategories, duas);
+
+    res.json(organizedData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Function to get subcategories for a given cat_id
+function getSubcategories(cat_id) {
+  return new Promise((resolve, reject) => {
+    db.all(
+      `SELECT * FROM sub_category WHERE cat_id = ${cat_id}`,
+      (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      }
+    );
+  });
+}
+
+// Function to get duas for a given cat_id
+function getDuas(cat_id) {
+  return new Promise((resolve, reject) => {
+    db.all(`SELECT * FROM dua WHERE cat_id = ${cat_id}`, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+}
+
+// Function to organize data by subcategory
+function organizeData(subcategories, duas) {
+  const organizedData = [];
+
+  subcategories.forEach((subcategory) => {
+    const dataForSubcategory = {
+      id: subcategory.id,
+      cat_id: subcategory.cat_id,
+      subcat_id: subcategory.subcat_id,
+      subcat_name_en: subcategory.subcat_name_en,
+      subcat_name_bn: subcategory.subcat_name_bn,
+      categories: [],
+    };
+
+    const duasForSubcategory = duas.filter(
+      (dua) => dua.subcat_id === subcategory.subcat_id
+    );
+
+    dataForSubcategory.categories = duasForSubcategory;
+
+    organizedData.push(dataForSubcategory);
+  });
+
+  return organizedData;
+}
+
 // Get Category and SubCategory Wise Dua
 app.get("/api/dua/:cat_id/:subcat_id", (req, res) => {
   const cat_id = req.params.cat_id;
